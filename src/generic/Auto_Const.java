6f2c5490cf28c704884115923197fcf9b8791d29
Swati <swatisharma1517@gmail.com>
2019-08-06 23:23:30 +0530

    AC

---
+++ src/generic/Auto_Const.java
@@ -0,0 +1,16 @@
+package generic;
+
+public interface Auto_Const 
+{
+
+	String GECKO_KEY="webdriver.gecko.driver";
+	String GECKO_VALUE="./software/geckodriver.exe";
+//	String CHROME_KEY="webdriver.chrome.driver";
+//	String CHROME_VALUE="./software/chromedriver.exe";
+
+	String PATH="./excel/test.xlsx";
+	
+
+	
+	
+}
